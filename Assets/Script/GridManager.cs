4a27ea3c0e84302cb8b10e3e88cf2564782d77b0
Noa <lqian062@gmail.com>
2026-06-22 09:10:56 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/Script/GridManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+
+    public float width = 10f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+}
