019784963f792180bc4e5ce401c5449569aa656d
MAETEL\2247020 <lqian062@gmail.com>
2026-06-25 16:21:53 -0800

    TurnManagerの作成、プレイヤーの入力と敵/プレイヤーの一斉実行処理

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,37 +3,77 @@ using static GridManager;
 
 public class Player : MonoBehaviour
 {
+
+    //グリッド情報
     GridManager gridManager;
     public int gridX;
     public int gridY;
 
-    private void Start()
+    //入力処理
+    public Vector2Int lastDirection;//最後の入力方向
+
+    void Start()
     {
+        TurnManager.Instance.SetPlayer(this);
         SnapToGrid();
     }
 
     //座標の設定
     void SnapToGrid() { transform.position = gridManager.GridToWorld(gridX, gridY); }
-    //移動処理
-    public bool PlayerMove(Vector2Int direction)
+    //移動入力処理
+    public void PlayerInput(Vector2Int direction)
     {
+        Vector2Int dir = Vector2Int.zero;
+        //入力
+        if (Input.GetKeyDown(KeyCode.W)) { dir = Vector2Int.up; }
+        if (Input.GetKeyDown(KeyCode.A)) { dir = Vector2Int.left; }
+        if (Input.GetKeyDown(KeyCode.S)) { dir = Vector2Int.down; }
+        if (Input.GetKeyDown(KeyCode.D)) { dir = Vector2Int.right; }
+
+        //directionが変わったら通知
+        if(dir != Vector2Int.zero)
+        {
+            TurnManager.Instance.isPlayerInput(dir);
+        }
+    }
+    //左右上下のセルに移動可能かを取得
+    public bool CanMove(Vector2Int direction)
+    {
+        //セル情報の取得
         int targetX = gridX + direction.x;
         int targetY = gridY + direction.y;
-
         Cell targetCell = gridManager.GetCell(targetX, targetY);
-        //歩けなかったらfalseを返すよん
-        if (targetCell == null ||! targetCell.isWalk) return false;
 
-        //位置変換
-        gridX = targetCell.x;
-        gridY = targetCell.y;
+        if (targetCell == null || !targetCell.isWalk) return false;
+        return true;
+    }
+    //移動方向の保存
+    public void SetMoveDirection(Vector2Int direction) { lastDirection = direction; }
+    //移動の実行
+    public void PlayerMove()
+    {
+        int targetX = gridX + lastDirection.x;
+        int targetY = gridY + lastDirection.y;
+
+        //再度判定を取る（ダメージ判定のない壁になる敵が出てきた時用）
+        var targetCell = gridManager.GetCell(targetX, targetY);
+        if (targetCell == null || !targetCell.isWalk)
+        {
+            MoveCancel(targetCell);
+            return;
+        }
+
+        //位置変更
+        gridX = targetX;
+        gridY = targetY;
         SnapToGrid();
 
-        //マネージャーに通知
-        TurnManager.Instance.OnPlayerMoved();
+    }
 
-        return true;
-
+    private void MoveCancel(GridManager.Cell cancelCell)
+    {
+        //ダメージ処理などの分岐は後で書く
+        Debug.Log("移動がキャンセルされました");
     }
 
 }
--- Assets/Script/TurnManager.cs
+++ Assets/Script/TurnManager.cs
@@ -1,5 +1,57 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TurnManager : MonoBehaviour
 {
+
+    //基本設定
+    public static TurnManager Instance { get; private set; }
+    public enum TurnState 
+    {
+        Wait,   //プレイヤーの入力待機
+        Action, //実行処理
+    }
+    public TurnState turnState = TurnState.Wait;
+
+    //キャラクター
+    private List<Enemy> enemies = new List<Enemy>();
+    private Player player;
+
+    void Awake()
+    {
+        //シングルトン化
+        if(Instance!=null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    //プレイヤーの登録
+    public void SetPlayer(Player p) { player = p; }
+    //敵の登録
+    public void AddEnemy(Enemy enemy) { enemies.Add(enemy); }
+    //敵の登録解除
+    public void RemoveEnemy(Enemy enemy) { enemies.Remove(enemy); }
+    //プレイヤーの移動入力通知
+    public void isPlayerInput(Vector2Int direction)
+    {
+        if ( turnState != TurnState.Wait) return;
+
+        if (!player.CanMove(direction)) return;
+
+        turnState = TurnState.Action;
+
+        //移動方向を設定
+        player.SetMoveDirection(direction);
+
+        //実行
+        foreach(var enemy in enemies) { enemy.EnemyMove(); }
+        player.PlayerMove();
+
+        turnState = TurnState.Wait;
+    }
+
 }
